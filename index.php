300780cf766bc219f2678ff2509452aa81126fa7
Gabriel Medina <medina.rj@live.com>
2021-02-19 21:53:59 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <link rel="icon" href="images_principal/thedog.ico" type="image/x-icon">
+    <!-- CSS Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <!-- CSS do Site -->
+    <link rel="stylesheet" href="main.css" type="text/css">
+
+      <title>The Dog Agência Digital</title>
+
+    <meta name="application-name" content="Site Institucional">
+    <meta name="author" content="Gabriel Medina">
+    <meta name="generator" content="Atom">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/2d270bfed8.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+
+  </head>
+  <body>
+    <div class="container-fluid" id="_site">
+
+      <section id="home">
+        <video autoplay="true" muted loop="true" role="presentation" preload="auto" playinline tabindex="-1">
+          <source src="images_principal/office.mp4" type="video/mp4">
+        </video>
+      </section>
+
+      <section id="servicos">
+
+      </section>
+
+      <section id="quem">
+
+      </section>
+
+      <section id="tecnologia">
+
+      </section>
+
+      <section id="contato">
+
+      </section>
+
+    </div>
+
+  </body>
+</html>
